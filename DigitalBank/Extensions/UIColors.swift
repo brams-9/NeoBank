55aaeb038054e7ab701742e068fddfaca6d43530
brams-9 <bramantyarahman@gmail.com>
2024-07-16 23:52:01 +0700

    feat: setup app colors

---
+++ DigitalBank/Extensions/UIColors.swift
@@ -0,0 +1,16 @@
+//
+//  UIColors.swift
+//  DigitalBank
+//
+//  Created by Rahman Bramantya on 16/07/24.
+//
+
+import UIKit
+
+extension UIColor {
+    static let growthColor = UIColor(named: "GrowthColor")
+    static let tintColor = UIColor(named: "TintColor")
+    static let titleColor = UIColor(named: "TitleColor")
+    static let subtitleColor = UIColor(named: "SubtitleColor")
+    static let linkColor = UIColor(named: "LinkColor")
+}
