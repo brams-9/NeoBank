078e1a91f6af50cf4bf57b47a9862fce8c816d1d
brams-9 <56192259+brams-9@users.noreply.github.com>
2024-07-23 14:28:50 +0700

    chore: delete Pods directory

--- Pods/Target Support Files/Pods-DigitalBank/Pods-DigitalBank-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DigitalBank : NSObject
-@end
-@implementation PodsDummy_Pods_DigitalBank
-@end
--- Pods/Target Support Files/Pods-DigitalBankTests/Pods-DigitalBankTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DigitalBankTests : NSObject
-@end
-@implementation PodsDummy_Pods_DigitalBankTests
-@end
